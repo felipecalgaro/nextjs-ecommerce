1d8f24dbfedf980035dbf29d367a2f4ff3906863
Felipe <pipecalgaro@gmail.com>
2024-05-23 16:16:00 -0300

    feat: create admin products page

---
+++ src/app/admin/products/[id]/download/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/db/prisma";
+import { notFound } from "next/navigation";
+import fs from "fs/promises";
+import { date } from "zod";
+
+export async function GET(
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) {
+  const product = await prisma.product.findUnique({
+    where: { id },
+    select: { filePath: true, name: true },
+  });
+
+  if (product == null) return notFound();
+
+  const { size } = await fs.stat(product.filePath);
+  const file = await fs.readFile(product.filePath);
+  const extension = product.filePath.split(".").pop();
+
+  return new NextResponse(file, {
+    headers: {
+      "Content-Disposition": `attachment; filename="${product.name}.${extension}"`,
+      "Content-Length": size.toString(),
+    },
+  });
+}
